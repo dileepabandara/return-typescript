0ed6a95ff722fd2d32dda4a12edbdfc5b0a9082b
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-02-04 12:46:47 +0530

    Update 01_helloworld.ts

--- 01_helloworld.ts
+++ 01_helloworld.ts
@@ -1,2 +1,11 @@
-let message: string = 'Hello, World!';
-console.log(message);
+let message1: string = 'Message 1 = Hello, World!';
+console.log(message1);
+
+const message2 = "Message 2 = Hello World"
+console.log(message2)
+
+var message3:string = "Message 3 = Hello World" 
+console.log(message3)
+
+var message4 = "Message 4 = Hello World";
+console.log(message4);
