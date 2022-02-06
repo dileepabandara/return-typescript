6e7bc396222e0f5cc52557cf289cb5b17773b320
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-02-06 22:57:19 +0530

    Create 03_numbers.ts

---
+++ 03_numbers.ts
@@ -0,0 +1,27 @@
+// Decimal numbers
+let counter: number = 0;
+let x: number = 100, 
+    y: number = 200;
+
+// Binary Numbers
+let bin = 0b100;
+let anotherBin: number = 0B010;
+
+// Octal Numbers
+let octal: number = 0o10;
+
+// Hexadecimal numbers
+let hexadecimal: number = 0XA;
+
+// Big Integers
+let big: bigint = 9007199254740991n;
+
+
+console.log('counter: ' + counter);
+console.log('x: ' + x);
+console.log('y: ' + y);
+console.log('bin: ' + bin);
+console.log('anotherBin: ' + anotherBin);
+console.log('octal: ' + octal);
+console.log('hexadecimal: ' + hexadecimal);
+console.log('big: ' + big);
